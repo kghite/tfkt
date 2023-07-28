8f74d33a99032bda66e5f7bc49abb5fad05aac7b
kghite <kghite@gmail.com>
2023-07-29 01:07:25 +0200

    Working leaflet base map

--- planner_gis/urls.py
+++ planner_gis/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view()),
-    path('places_data/', views.places_dataset, name='my_places'),
+    path('gis_data', views.places_dataset, name='gis_data'),
 ]
